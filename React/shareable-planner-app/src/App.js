b9de119002611d6c1bb2bdaf39a37f0fa0a8a05a
Neer Patel <neerpatel@Neers-MacBook-Pro.local>
2022-09-29 17:18:07 -0400

    Added React directory containing the shareable-planner-app.

---
+++ React/shareable-planner-app/src/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1 id = "Title">
+      Shareable Planner
+    </h1>
+    <p>
+      Work in Progress!
+    </p>
+
+
+    </>
+  );
+}
+
+export default App;
