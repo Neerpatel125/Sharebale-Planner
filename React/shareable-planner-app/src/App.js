0693cc5ae014886e4ea3d6ab8a17f50a19d0a051
Neer Patel <neerpatel@Neers-MacBook-Pro.local>
2022-10-10 00:07:35 -0400

    Added CSS for the Calendar, made "Events" and "Add Events" boxes.

--- React/shareable-planner-app/src/App.js
+++ React/shareable-planner-app/src/App.js
@@ -1,18 +1,47 @@
-import React, { useState } from "react"
-import Calendar from "react-calendar"
+import React, { useState } from "react";
+import Calendar from "react-calendar";
 import './App.css';
 
 function App() {
-  const [value, onChange] = useState(new Date());
+  const value = useState(new Date());
   
   return (
     <>
+    {/* HTML for the Title */}
     <h1 class = "Title">
       Shareable Planner
     </h1>
-    <div>
-      <Calendar onChange = {onChange} value = {value}></Calendar>
+
+    {/* HTML for the Calendar */}
+    <div class = "calendar">
+      <Calendar value={value} minDetail="month" 
+        next2Label={null} prev2Label={null} showNeighboringMonth={false} 
+        ></Calendar>
     </div>
+
+    {/* HTML for the Events List Box */}
+    <div class="eventsBox">
+      <div class="eventsBox__title">
+        <h4>Events for Selected Day</h4>
+      </div>
+      <ul class="eventsList"></ul>
+    </div>
+
+    {/* HTML for the Add Events Box */}
+    <div class="addBox">
+      <div class="addBox__title">
+        <h4>Add Events</h4>
+      </div>
+      <div class="addBox__content">
+        <h4>Enter the name of the event: </h4>
+        <input placeholder="Event Name"></input>
+        <h4>Enter names of people to invite</h4>
+        <input placeholder="Name1, Name2, ..."></input>
+        <br></br> <br></br>
+        <button class="addEventButton">Add Event</button>
+      </div>
+    </div>
+
     </>
   );
 }
