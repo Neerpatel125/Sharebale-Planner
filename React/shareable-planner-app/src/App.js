7d2f996e7c2dbafe7f30b07401924ff4b1f64d05
Neer Patel <neerpatel@Neers-MacBook-Pro.local>
2022-12-04 15:12:23 -0500

    Improved organization of code.

--- React/shareable-planner-app/src/App.js
+++ React/shareable-planner-app/src/App.js
@@ -1,79 +1,14 @@
-import React, { useState, useEffect } from "react";
-import Calendar from "react-calendar";
+import React from "react";
 import './App.css';
-import EventsList from "./EventsList";
+import HomePage from "./HomePage"
+
 
 function App() {
-  /* Get the current date for the calendar */
-  const [selectedDay, setSelectedDay] = useState(new Date());
-  const splitSelectedDay = selectedDay.toString().split(" ").slice(0, 4);
-  const [stringSelectedDay, setStringSelectedDay] = useState(splitSelectedDay.map( (word) => word = word + " "));
-  /* Open the GMU events website function */
-  const gmuEvents = () => {
-    window.open("https://mason360.gmu.edu/events", "_blank");
-  }
-  /* Store input information for adding events */
-  const [eventName, setEventName] = useState("");
-  const [eventTime, setEventTime] = useState("");
-  const [eventInvites, setEventInvites] = useState("");
-  const [myEvents, setMyEvents] = useState([]); /* Temporary, since currently cannot add to database */
-
-  function handleAddEvent(){
-    /* Currently just adds the events to myEvents list, but should send them to the database */
-    const event = {eventName, eventTime, eventInvites};
-    setMyEvents((prevEvents) =>{
-      return [...prevEvents, event];
-    });
-  }
-  /* Updates stringSelectedDay whenver selectedDay changes.  
-    Note: When selectedDay changes, you should load the events for the new day from the databse into myEvents,
-    then <EventsList events={myEvents}/> will update the list to display events for the new day upon re-rendering. 
-  */
-  useEffect(() => {
-    const splitSelectedDay = selectedDay.toString().split(" ").slice(0, 4);
-    setStringSelectedDay(splitSelectedDay.map( (word) => word = word + " "));
-  }, [selectedDay]);
-  
   return(
     <>
-    {/* HTML for the Title */}
-    <h1 class = "Title">Shareable Planner</h1>
-
-    {/* HTML for the Calendar */}
-    <div class = "calendar">
-      <Calendar value={selectedDay} minDetail="month" 
-        next2Label={null} prev2Label={null} showNeighboringMonth={false} 
-        onClickDay={(day)=> setSelectedDay(day)}> </Calendar>
-    </div>
-
-    {/* HTML for the Events List Box */}
-    <div class="eventsBox">
-      <div class="eventsBox__title">
-        <h2>Events for {stringSelectedDay}</h2>
-      </div>
-      <EventsList events={myEvents} setEvents={setMyEvents}/>
-    </div>
-
-    {/* HTML for the Add Events Box */}
-    <div class="addBox">
-      <div class="addBox__title">
-        <h2>Add Events for {stringSelectedDay}</h2>
-      </div>
-      <div class="addBox__content">
-        <h3>Enter the name of the event: </h3>
-        <input onChange={(e) => setEventName(e.target.value)} placeholder="Event Name" value={eventName} ></input>
-        <h3>Enter the time of the event: </h3>
-        <input onChange={(e) => setEventTime(e.target.value)} placeholder="12:00 PM to 1:00 PM" value={eventTime} ></input>
-        <h3>Enter names of people to invite</h3>
-        <input onChange={(e) => setEventInvites(e.target.value)} placeholder="Name1, Name2, ..." value={eventInvites}></input>
-        <br></br> <br></br>
-        <button onClick={handleAddEvent} class="addEventButton">Add Event</button>
-      </div>
-    </div>
-
-    {/* HTML for GMU Events Button */}
-    <button onClick={() => gmuEvents()} class="gmuEventsButton">GMU Events</button>
+      <HomePage/>
     </>
+
   );
 }
 
